8e060f25a0dd157ac1f398a82048622dd1172f73
parkjs5143 <parkjs5143@gmail.com>
2021-08-31 10:26:52 +0900

    스프링 첫 깃허브 연결!

---
+++ src/main/java/com/koreait/springboard/SpringboardApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.springboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/springboard/SpringboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.springboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
